417452816954e22b3c220b9ce2dc49dd1ac00a35
williame <williame_lima@hotmail.com>
2022-01-27 17:51:11 -0300

    correções sinais vitais casas decimais peso

--- src/components/IncrementDecrement.tsx
+++ src/components/IncrementDecrement.tsx
@@ -1,6 +1,6 @@
 import React, { useState, memo, useCallback, useEffect } from 'react';
-import { StyleSheet, View, Pressable, Dimensions, Text, TextInput, TextInputProps } from 'react-native';
-import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
+import { StyleSheet, View, Pressable, Dimensions, Text } from 'react-native';
+import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
 import Adicao from '../assets/svg/math-plus.svg';
 import Subtracao from '../assets/svg/math-minus.svg';
 import { MaskedTextInput } from 'react-native-mask-text';
@@ -8,49 +8,56 @@ interface Props {
     RangerValue: number;
     medida: string;
     min: number;
-    max: number
+    max: number;
     setRangerValue(value: number): void;
-};
+}
 
-const IncrementDecrement: React.FC<Props> = ({ RangerValue = 0, setRangerValue, medida, min= 0, max= 100 }: Props) => {
+const IncrementDecrement: React.FC<Props> = ({
+    RangerValue = 0,
+    setRangerValue,
+    medida,
+    min = 0,
+    max = 100,
+}: Props) => {
+    const [inputValue, setInputValue] = useState(RangerValue.toString());
 
-    const [ inputValue, setInputValue ] = useState(RangerValue.toString());
-
-    const size = Dimensions.get('screen').height / 40
+    const size = Dimensions.get('screen').height / 40;
 
     const inc_Dec = (tipo: string) => {
-
         var value: number = 1;
 
         switch (medida) {
-            case '°C': {
-                value = 0.1;
-                let ranger: number = RangerValue;
-                if (tipo === 'soma') {
-                    setRangerValue(ranger + value);
-                } else if (tipo === 'subtracao') {
-                    setRangerValue(ranger - value);
+            case '°C':
+                {
+                    value = 0.1;
+                    let ranger: number = RangerValue;
+                    if (tipo === 'soma') {
+                        setRangerValue(ranger + value);
+                    } else if (tipo === 'subtracao') {
+                        setRangerValue(ranger - value);
+                    }
                 }
-            }
                 break;
-            case 'kg': {
-                value = 0.1;
-                let ranger: number = RangerValue;
-                if (tipo === 'soma') {
-                    setRangerValue(ranger + value);
-                } else if (tipo === 'subtracao') {
-                    setRangerValue(ranger - value);
+            case 'kg':
+                {
+                    value = 0.1;
+                    let ranger: number = RangerValue;
+                    if (tipo === 'soma') {
+                        setRangerValue(ranger + value);
+                    } else if (tipo === 'subtracao') {
+                        setRangerValue(ranger - value);
+                    }
                 }
-            }
                 break;
             default:
                 if (tipo === 'soma') {
                     setRangerValue(RangerValue + value);
                 } else if (tipo === 'subtracao') {
                     setRangerValue(RangerValue - value);
-                } break;
+                }
+                break;
         }
-    }
+    };
 
     const setValue = useCallback((value: string) => {
         if (value) {
@@ -64,33 +71,44 @@ const IncrementDecrement: React.FC<Props> = ({ RangerValue = 0, setRangerValue, 
     }, []);
 
     useEffect(() => {
-        setInputValue(RangerValue.toString())
+        setInputValue(RangerValue.toString());
     }, [RangerValue]);
 
     const setMask = (): string => {
         switch (medida) {
-            case '°C': return "99.9";
+            case '°C':
+                return '99.9';
                 break;
-            case 'kg': return "999.9";
+            case 'kg':
+                if(inputValue.length > 3){
+                    return '999.9';
+                }else{
+                    return '99.9';     
+                }
                 break;
-            case 'SpO²': return "999";
+            case 'SpO²':
+                return '999';
                 break;
-            case 'cm': return "999";
+            case 'cm':
+                return '999';
                 break;
-            default: return "[9999]";
+            default:
+                return '[9999]';
         }
-    }
+    };
 
     return (
         <View style={styles.Container}>
-            <Pressable style={styles.btnInc} onPress={() => inc_Dec('subtracao')}>
+            <Pressable
+                style={styles.btnInc}
+                onPress={() => inc_Dec('subtracao')}>
                 <Subtracao fill={'#748080'} width={size} height={size} />
             </Pressable>
             <MaskedTextInput
                 mask={setMask()}
                 keyboardType={'numeric'}
                 value={inputValue}
-                maxLength={4}
+                maxLength={5}
                 style={styles.valueInput}
                 onChangeText={(text, rawText) => setInputValue(text)}
                 onBlur={() => setValue(inputValue.toString())}
@@ -102,10 +120,12 @@ const IncrementDecrement: React.FC<Props> = ({ RangerValue = 0, setRangerValue, 
                 <Adicao fill={'#748080'} width={size} height={size} />
             </Pressable>
         </View>
-    )
-}
+    );
+};
 
-{/* {Number.isInteger(RangerValue) ? RangerValue : RangerValue.toFixed(1)} */ }
+{
+    /* {Number.isInteger(RangerValue) ? RangerValue : RangerValue.toFixed(1)} */
+}
 
 const styles = StyleSheet.create({
     Container: {
@@ -113,19 +133,19 @@ const styles = StyleSheet.create({
         alignSelf: 'center',
         justifyContent: 'center',
         alignItems: 'center',
-        zIndex: 1
+        zIndex: 1,
     },
     valueInput: {
         width: Dimensions.get('screen').width / 7,
         fontSize: RFValue(20, 680),
         textAlign: 'center',
-        color: '#7C9292'
+        color: '#7C9292',
     },
     btnInc: {
         width: 40,
         height: 50,
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
     },
     textInc: {
         color: '#7C9292',
@@ -135,7 +155,7 @@ const styles = StyleSheet.create({
     text: {
         color: '#7C9292',
         fontSize: RFValue(14, 680),
-    }
+    },
 });
 
 export default memo(IncrementDecrement);
